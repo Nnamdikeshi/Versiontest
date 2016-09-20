3de596c91a032cd9c0484cbcef3b6d083b278059
Nc0614dn <Me@me.com>
2016-09-20 11:29:14 -0500

    Verseion Control Stuff Created Dinosaur class and tested roar meesage

---
+++ src/com/company/Dinosaur.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by nc0614dn on 9/20/2016.
+ */
+public class Dinosaur {
+    public void roar() {
+        System.out.println("GRRR!!");
+    }
+}
---
+++ src/com/company/DinosaurSuperior.java
@@ -0,0 +1,17 @@
+package com.company;
+
+/**
+ * Created by nc0614dn on 9/20/2016.
+ */
+public class DinosaurSuperior {
+    public static void main(String[] args) {
+        Dinosaur t_rex = new Dinosaur();
+        Dinosaur velociraptor = new Dinosaur();
+        Dinosaur diplodocous = new Dinosaur();
+
+
+        t_rex.roar();
+        velociraptor.roar();
+        diplodocous.roar();
+    }
+}
